8d9c8f66fe514f3612d181e49a045ed493caa5a8
shinyay <shinya.com@gmail.com>
2024-08-13 18:55:30 +0900

    Add MVC related depenencies #4

---
+++ src/main/java/com/microsoft/shinyay/demo/controller/SimpleController.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Controller;
+
+package com.microsoft.shinyay.demo.controller;
+
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
