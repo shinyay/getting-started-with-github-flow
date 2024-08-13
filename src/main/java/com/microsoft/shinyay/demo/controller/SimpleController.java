f4d5a12232248e3409a6954d4c37bdf109283924
shinyay <shinya.com@gmail.com>
2024-08-13 19:13:44 +0900

    Add a Simple view model #4

--- src/main/java/com/microsoft/shinyay/demo/controller/SimpleController.java
+++ src/main/java/com/microsoft/shinyay/demo/controller/SimpleController.java
@@ -1,6 +1,9 @@
-import org.springframework.stereotype.Controller;
-
 package com.microsoft.shinyay.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class SimpleController {
