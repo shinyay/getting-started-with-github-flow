e8ac140e1059d838a0423ede63e9e469d8c8876e
shinyay <shinya.com@gmail.com>
2024-08-14 22:42:02 +0900

    Add EntityScan annotation

--- src/main/java/com/microsoft/shinyay/demo/DemoApplication.java
+++ src/main/java/com/microsoft/shinyay/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.microsoft.shinyay.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	@EnableJpaRepositories(basePackages = "com.microsoft.shinyay.demo.repository")
+	@EntityScan(basePackages = "com.microsoft.shinyay.demo.entity")
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
