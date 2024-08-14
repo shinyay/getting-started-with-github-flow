353b2bd9c00965c85da2626600b8e3bf32ab8df5
shinyay <shinya.com@gmail.com>
2024-08-14 22:29:46 +0900

    Add an Entitly class and package

---
+++ src/main/java/com/microsoft/shinyay/demo/entity/Book.java
@@ -0,0 +1,14 @@
+package com.microsoft.shinyay.demo.entity;
+
+import javax.persistence.Entity;
+// I will create a entity class for a book
+// This class will be used to represent a book in the database
+// The database will be created automatically by Spring Data JPA
+// The database will be an in-memory database
+// The database will be H2
+// The database will be created in the file system
+
+@Entity
+public class Book {
+
+}
