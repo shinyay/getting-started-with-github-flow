6575291712fa4692678aad87c88762e2e0ce6ce0
shinyay <shinya.com@gmail.com>
2024-08-14 22:41:30 +0900

    Add EnableJpaRepositories annotation

--- src/main/java/com/microsoft/shinyay/demo/DemoApplication.java
+++ src/main/java/com/microsoft/shinyay/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.microsoft.shinyay.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@EnableJpaRepositories(basePackages = "com.microsoft.shinyay.demo.repository")
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
